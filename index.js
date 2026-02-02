7b699cd70118f04ddc31dda674276c5b38d7d53b
Mahesa <mahesaagsa101@gmail.com>
2026-02-02 19:24:09 +0700

    menambahkan sistem matematika

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ let angka =40;
 for(let i =0;i<angka;i++){
     return console.log(i)
 }
+let masuk =Number(prompt("masukan akan angka:"));
+
+let random = Math.floor(Math.random()*100)+1;
+let hasil = masuk+random;
+alert(`angka anda ${masuk}
+    angka random: ${random}
+    TOTAL: ${hasil}`)
